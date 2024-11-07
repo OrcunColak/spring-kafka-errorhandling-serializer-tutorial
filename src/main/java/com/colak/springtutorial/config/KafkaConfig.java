f4495327403625d81ebb20b9fb5aaa4c70aec69a
Orçun Çolak <orcun.colak@hazelcast.com>
2024-11-07 11:22:22 +0300

    Add deserialization error test

--- src/main/java/com/colak/springtutorial/config/KafkaConfig.java
+++ src/main/java/com/colak/springtutorial/config/KafkaConfig.java
@@ -1,7 +1,5 @@
 package com.colak.springtutorial.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -9,10 +7,7 @@ import org.springframework.kafka.listener.CommonLoggingErrorHandler;
 
 @EnableKafka
 @Configuration
-@RequiredArgsConstructor
 public class KafkaConfig {
-
-    private final KafkaProperties kafkaProperties;
 
     @Bean
     public CommonLoggingErrorHandler errorHandler() {
--- src/test/java/com/colak/springtutorial/consumer/ConsumerTest.java
+++ src/test/java/com/colak/springtutorial/consumer/ConsumerTest.java
@@ -1,7 +1,30 @@
 package com.colak.springtutorial.consumer;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
+import java.util.concurrent.TimeUnit;
+
+@SpringBootTest(properties = {
+        "spring.kafka.consumer.auto-offset-reset=earliest",
+        "spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}"
+})
+@EmbeddedKafka(partitions = 1, topics = { Consumer.TOPIC_NAME })
 class ConsumerTest {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+
+    @Test
+    void testInvalidMessage() throws InterruptedException {
+        // Send an invalid message (e.g., malformed JSON) to trigger deserialization error
+        kafkaTemplate.send(Consumer.TOPIC_NAME, "invalid message");
+
+        TimeUnit.SECONDS.sleep(10);
+    }
 
 }
